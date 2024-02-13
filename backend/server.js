fc97b4c7e0cbb744b5afb134c4de25310d44a90b
omjavheri <javheri.om2003@gmail.com>
2024-02-14 01:23:43 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const express=require("express")
 const mysql=require("mysql")
 const cors=require("cors")
-
+require("dotenv").config();
 const app=express();
 app.use(cors());
 app.use(express.json());
 
 const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"samsung@6600",
-    database:"concert",
+    host:`${process.env.MYSQLHOST}`,
+    user:`${process.env.MYSQLUSER}`,
+    password:`${process.env.MYSQLPASSWORD}`,
+    database:`${process.env.MYSQLDATABASE}`,
     debug:true
 });
 
@@ -65,29 +65,6 @@ app.post('/Signup', (req, res) => {
         }
     });
 });
-// app.post('/CheckLogin', (req, res) => {
-//   // Dummy login status for demonstration purposes
-//   const isLoggedIn = true;
-
-//   // Assuming you want to check login status on the server
-//   const sql = "SELECT login_status FROM login WHERE login_status=1";
-
-//   db.query(sql, (err, data) => {
-//     if (err) {
-//       console.error("Error:", err);
-//       res.status(500).json({ message: "Internal Server Error" });
-//     } else {
-//       if (data.length > 0 || isLoggedIn) {
-//         // You have found a user matching the condition or using dummy login status
-//         res.status(200).json({ status: "success" });
-//       } else {
-//         // No user found
-//         res.status(401).json({ message: "Incorrect username or password" });
-//       }
-//     }
-//   });
-// });
-
 
 app.post('/Create', (req, res) => {
     
@@ -259,101 +236,6 @@ app.post('/ShowAndDelete', (req, res) => {
     }
   });
 });
-app.post('/ShowAndBook', (req, res) => {
-    
-  const sql = "INSERT INTO tms(tid ,username,password,ticket)VALUES(?,?,?,?);";
-  
-   
-    const tid=req.body.tid;
-    const username=req.body.username;
-    const password=req.body.password;
-    const ticket=req.body.ticket;
-    // const date=req.body.date;
-    // const time=req.body.time;
-    // const ticket=req.body.ticket;
-   
-  
-  db.query(sql, [tid,username,password,ticket], (err, data) => {
-    if (err) {
-      
-      req.setEncoding({err:err});
-      console.log("Error");
-    }
-    else{
-      if(data.length>0){
-          console.log("booked succesful")
-          res.send(data)
-
-      }
-      else{
-          res.send({message:"Incorrect username or password"})
-      }
-        
-
-    }
-    
-    
-  });
-
-});
-app.get('/Show', (req, res) => {
-  const sql = "SELECT * FROM cms;";
-  
- 
-  
-  db.query(sql,[], (err, data) => {
-    if (err) {
-      console.error("Error", err);
-      res.status(500).json({ message: "Failed to show data" });
-    } else {
-      if (data.length > 0) {
-        console.log("Data shown successfully");
-        res.json(data);
-      } else {
-        res.json({ message: "No data found" });
-      }
-    }
-  });
-});
-app.get('/Booked', (req, res) => {
-  const sql = "SELECT * FROM tms WHERE id = (SELECT MAX(id) FROM tms);";
-  
-  
-  db.query(sql, (err, data) => {
-    if (err) {
-      console.error("Error", err);
-      res.status(500).json({ message: "Failed to retrieve data" });
-    } else {
-      if (data.length > 0) {
-        console.log("Data retrieved successfully");
-        res.json(data);
-      } else {
-        res.json({ message: "No data found" });
-      }
-    }
-  });
-});
-
-
-app.post('/DelTicket', (req, res) => {
-  const sql = "DELETE FROM tms WHERE id=?;";
-  const id=req.body.id;
- 
-  
-  db.query(sql,[id], (err, data) => {
-    if (err) {
-      console.error("Error", err);
-      res.status(500).json({ message: "Failed to show data" });
-    } else {
-      if (data.length > 0) {
-        console.log("Data deleted successfully");
-        res.json(data);
-      } else {
-        res.json({ message: "No data found" });
-      }
-    }
-  });
-});
 
 
 
@@ -364,7 +246,7 @@ db.connect((err,values) => {
       return;
     }
     console.log('Connected to database!');
-    app.listen(8081, () => {
+    app.listen(`${process.env.MYSQLPORT}`, () => {
       console.log("Server is listening on port 8081",values);
     });
   });
